1f7541f39f76435fa41f403b22a9b94f6219f0d7
Unknown <imeraz_@hotmail.fr>
2018-04-26 12:17:47 +0200

    Filter by Actor and gender

--- app/src/main.js
+++ app/src/main.js
@@ -45,27 +45,24 @@ if (currentPage == "index") {
 }
 else if (currentPage == "sort") {
     var genres = [];
-    $('input[type="checkbox"]').change(function() {
+    $('input[type="checkbox"]').change(function () {
         $('input[name="genre"]').val('')
-        $('input[type="checkbox"]').each(function(elem) {
-            console.log($(this))
-            if($(this).checked) {
-                console.log( $('input[name="genre"]').val())
-                console.log($(this))
+        $('input[data-search="genre"]').each(function () {
+            var input = $(this)[0]
+            if (input.checked) {
                 $('input[name="genre"]').val(
-                    $('input[name="genre"]').val()+","+$(this)[0].val()
+                    $('input[name="genre"]').val() + "," + input.value
                 )
             }
         });
-        console.log($('input[name="genre"]').val())
-    }) 
-
+    })
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=6b2d1b79a395d6c1c9c9a72f5afab091&language=fr&query="
     if ($.urlParam('search')) {
         var movie = $.urlParam('search')
         $.ajax({
             "async": true,
             "crossDomain": true,
-            "url": "https://api.themoviedb.org/3/search/movie?api_key=6b2d1b79a395d6c1c9c9a72f5afab091&language=fr&query=" + movie + "&page=1",
+            "url": url + movie + "&page=1",
             "method": "GET",
             "headers": {},
             "data": "{}"
@@ -87,23 +84,29 @@ else if (currentPage == "sort") {
             "headers": {},
             "data": "{}"
         }).done(function (response) {
-            idActeur = response.results[0].id
+            if ($.urlParam('acteur') != 0) {
+                idActeur = "&with_cast=" + response.results[0].id
+            }
+            else { idActeur = "" }
+            if ($.urlParam('genre') != 0) {
+                var genre = "&with_genres=" + $.urlParam('genre')
+            }
+            else { genre = "" }
+            var url = "https://api.themoviedb.org/3/discover/movie?api_key=6b2d1b79a395d6c1c9c9a72f5afab091&language=fr&sort_by=release_date.desc&page=1"
             $.ajax({
                 "async": true,
                 "crossDomain": true,
-                "url": "https://api.themoviedb.org/3/discover/movie?api_key=6b2d1b79a395d6c1c9c9a72f5afab091&language=fr&sort_by=release_date.desc&page=1&with_cast=" + idActeur,
+                "url": url + idActeur + genre,
                 "method": "GET",
                 "headers": {},
                 "data": "{}"
             }).done(function (response) {
                 var details = ""
                 response.results.map(function (elem) {
-                    details += '<article><a href="single.html?id=' + elem.id + '"><img src="https://image.tmdb.org/t/p/w500' + elem.poster_path + '"><h1>' + elem.title + '</h1></a></article>';
+                    details += '<article><a href="single.html?id=' + elem.id + '"><img src="https://image.tmdb.org/t/p/w500' + elem.poster_path + '"></a><h1>' + elem.title + '</h1></article>';
                     $('div.list').html(details)
                 })
             });
         });
     }
 }
-
-console.log($.urlParam('genre'));
